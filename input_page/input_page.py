040cdc1262b0734a45f1041991da167313cf04bc
土山晃平 <tsuchiyamaakirataira@tsuchiyamaakirahyous-MacBook-Pro.local>
2021-10-25 14:17:15 +0900

    botandekieruyo

--- input_page/input_page.py
+++ input_page/input_page.py
@@ -4,9 +4,6 @@ import sys
 import tkinter as tk
 from tkinter.constants import N
 
-root = tk.Tk()
-root.title(u"input")
-root.geometry("1500x750")
 
 #入力データいれ
 class input_data():
@@ -17,7 +14,7 @@ class input_data():
         self.ponta = d
         self.dugong = e
         self.check()
-        
+
     #無効な入力を弾く(ただデモではいらないなと思ったので後で)
     def check(self):
         name = self.name
@@ -27,96 +24,133 @@ class input_data():
         dugong = self.dugong
         if int(number)>100 or int(number)<0:
             print("無効な入力です!!")
+        else:
+            print("正しい入力です。")
 
-#データ転送用関数
-def Send_data(event):
-  value_name = EditBox1.get()
-  value_num = EditBox2.get()
-  value_children_num = EditBox3.get()
-  value_ponta = EditBox4.get()
-  value_dugong = EditBox5.get()
-  data=input_data(value_name,value_num,value_children_num,value_ponta,value_dugong)
-  #debug
-  print(data.name)
-  print(data.number)
-  print(data.dugong)
-  #
-  EditBox1.delete(0,tk.END)
-  EditBox2.delete(0,tk.END)
-  EditBox3.delete(0,tk.END)
-  EditBox4.delete(0,tk.END)
-  EditBox5.delete(0,tk.END)
-  #
-  return data
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.frame = master
+        self.frame.title(u"input")
+        self.frame.geometry("1500x750")
+        #タイトル(mainと同一にする)
+        string_title = tk.Label(
+            text=u"rityo_math（プロダクト名）", 
+            foreground='#00b0d9', 
+            background='#FFFFFF',
+            font=("Helvetica", "50", "bold"),
+            height=1,
+            width=30
+        )
+        string_title.pack(anchor=tk.N,side=tk.TOP)
 
-#タイトル(mainと同一にする)
-string_title = tk.Label(
-    text=u"rityo_math（プロダクト名）", 
-    foreground='#00b0d9', 
-    background='#FFFFFF',
-    font=("Helvetica", "50", "bold"),
-    height=1,
-    width=30
-)
-string_title.pack(anchor=tk.N,side=tk.TOP)
+        #説明文
+        string_directions = tk.Label(
+            text=u"必要事項を記入後、<決定>ボタンを押してください。",
+            font=("Helvetica", "30", "bold"),
+            bg="#edf7f5",
+            highlightthickness=3, 
+            highlightcolor="red",
+            takefocus=True
+        )
+        string_directions.pack(anchor=tk.N,side=tk.TOP)
 
-#説明文
-string_directions = tk.Label(
-    text=u"必要事項を記入後、<決定>ボタンを押してください。",
-    font=("Helvetica", "30", "bold"),
-    bg="#edf7f5",
-    highlightthickness=3, 
-    highlightcolor="red",
-    takefocus=True
-)
-string_directions.pack(anchor=tk.N,side=tk.TOP)
+        self.create_weget()
+
+        #オマケ
+        string_explain = tk.Label(
+            text=u"Have a good reservation!!", 
+            foreground='#00b0d9', 
+            background='#FFFFFF',
+            font=("Helvetica", "30", "bold"),
+            height=1,
+            width=30
+        )
+        string_explain.pack(anchor=tk.N,side=tk.BOTTOM)
+
+    def create_weget(self):
+        # ウィジェット変数を生成
+        #self.sv = tk.StringVar()
+        #self.sv.set("")
+
+        #氏名
+        self.Static1 = tk.Label(text=u'氏名',font = ("Helvetica", "20"))
+        self.Static1.pack()
+        self.EditBox1 = tk.Entry(width=25,textvariable=tk.StringVar())
+        self.EditBox1.pack()
+
+        #人数
+        self.Static2 = tk.Label(text=u'人数',font = ("Helvetica", "20"))
+        self.Static2.pack()
+        self.EditBox2 = tk.Entry(width=25,textvariable=tk.IntVar())
+        self.EditBox2.pack()
+
+        #うち子供
+        self.Static3 = tk.Label(text=u'子供の人数',font = ("Helvetica", "20"))
+        self.Static3.pack()
+        self.EditBox3 = tk.Entry(width=25,textvariable=tk.IntVar())
+        self.EditBox3.pack()
+
+        #Ponta
+        self.Static4 = tk.Label(text=u'Ponta',font = ("Helvetica", "20"))
+        self.Static4.pack()
+        self.EditBox4 = tk.Entry(width=25,textvariable=tk.IntVar())
+        self.EditBox4.pack()
+
+        #dugong
+        self.Static5 = tk.Label(text=u'dugong',font = ("Helvetica", "20"))
+        self.Static5.pack()
+        self.EditBox5 = tk.Entry(width=25,textvariable=tk.IntVar())
+        self.EditBox5.pack()
+
+        #ボタン
+        self.Button = tk.Button(text=u'決定')
+        self.Button.bind("<Button-1>",self.decision) 
+        self.Button.pack()
 
 
-#氏名
-Static1 = tk.Label(text=u'氏名',font = ("Helvetica", "20"))
-Static1.pack()
-EditBox1 = tk.Entry(width=25)
-EditBox1.pack()
 
-#人数
-Static2 = tk.Label(text=u'人数',font = ("Helvetica", "20"))
-Static2.pack()
-EditBox2 = tk.Entry(width=25)
-EditBox2.pack()
+    def decision(self,event):
+        self.Send_data(event)
+        self.close_window(event)
 
-#うち子供
-Static3 = tk.Label(text=u'子供の人数',font = ("Helvetica", "20"))
-Static3.pack()
-EditBox3 = tk.Entry(width=25)
-EditBox3.pack()
+    #window閉じる関数
+    def close_window(self,event):
+        self.frame.destroy()
 
-#Ponta
-Static4 = tk.Label(text=u'Ponta',font = ("Helvetica", "20"))
-Static4.pack()
-EditBox4 = tk.Entry(width=25)
-EditBox4.pack()
+    #データ転送用関数
+    def Send_data(self,event):
+        value_name = self.EditBox1.get()
+        value_num = self.EditBox2.get()
+        value_children_num = self.EditBox3.get()
+        value_ponta = self.EditBox4.get()
+        value_dugong = self.EditBox5.get()
+        data=input_data(value_name,value_num,value_children_num,value_ponta,value_dugong)
+        #debug
+        print(data.name)
+        print(data.number)
+        print(data.dugong)
+        #
+        self.EditBox1.delete(0,tk.END)
+        self.EditBox2.delete(0,tk.END)
+        self.EditBox3.delete(0,tk.END)
+        self.EditBox4.delete(0,tk.END)
+        self.EditBox5.delete(0,tk.END)
+        #
+        return data
 
-#dugong
-Static5 = tk.Label(text=u'dugong',font = ("Helvetica", "20"))
-Static5.pack()
-EditBox5 = tk.Entry(width=25)
-EditBox5.pack()
+#リターンで値を返せば良い
+#決定ボタンで閉じるように#まあdone
+#実行タイミングを制御できるように
+
+# 適当に継承する
+# import ButtonData
+def main():
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
 
 
-#ボタン
-Button = tk.Button(text=u'決定')
-Button.bind("<Button-1>",Send_data) 
-Button.pack()
-
-#オマケ
-string_explain = tk.Label(
-    text=u"Have a good reservation!!", 
-    foreground='#00b0d9', 
-    background='#FFFFFF',
-    font=("Helvetica", "30", "bold"),
-    height=1,
-    width=30
-)
-string_explain.pack(anchor=tk.N,side=tk.BOTTOM)
-
-root.mainloop()
+if __name__ == '__main__': # このファイルが他のファイルから直接呼ばれたときだけ以下を呼ぶ
+    # mainを呼ぶ
+    main()
