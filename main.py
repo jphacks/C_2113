4a3abe9f3353a1361b84fae06ce8bc1e7ff27bd0
Kentaro Seki <kentaro.kpc@gmail.com>
2021-10-29 14:54:41 +0900

    flagを適切に設定

--- main.py
+++ main.py
@@ -165,8 +165,9 @@ def test_gui_integration():
 
     root.mainloop()
 
-if __name__ == '__main__':
-   main(debug_mode=True, skip_input=True, tts_skip=False, stt_skip=False) 
+if __name__ == '__main__': 
+    main(debug_mode=True, skip_input=False, tts_skip=True, stt_skip=True) 
+
 
 
    # test_gui_integration()
